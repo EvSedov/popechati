aa50d231942ece17894229cbf377832514649058
sedovev <sedovev@rgn.gov.ru>
2025-05-14 01:14:18 +0700

    adds font Commissioner and ExamplesTShirt sections

--- resources/js/Components/ExamplesTShirts.vue
+++ resources/js/Components/ExamplesTShirts.vue
@@ -1,9 +1,48 @@
 <script lang="ts" setup></script>
 <template>
-    <div class="h-[705px] w-full pt-[175px]">
-        <h2 class="text-center text-[42px] font-bold text-black">
+    <div class="h-[705px] w-full pt-[160px]">
+        <h2 class="mb-10 text-center text-[42px] font-semibold text-[#2C2C2C]">
             Примеры: Футболок
         </h2>
+        <div class="mx-auto flex w-fit gap-5.5">
+            <div
+                class="relative h-89 w-89 overflow-hidden rounded-xl bg-[url(/resources/images/examp-1.jpg)]"
+            >
+                <div
+                    class="absolute top-0 right-0 bottom-0 left-0 flex items-end bg-black/25"
+                >
+                    <p class="mb-4 ml-5 text-lg font-semibold text-white">
+                        🔥 Сексуальная футблока
+                    </p>
+                </div>
+            </div>
+            <div
+                class="relative h-89 w-89 overflow-hidden rounded-xl bg-[url(/resources/images/examp-2.jpg)]"
+            >
+                <div
+                    class="absolute top-0 right-0 bottom-0 left-0 flex items-end bg-black/25"
+                >
+                    <p class="mb-4 ml-5 text-lg font-semibold text-white">
+                        🔥 Аниме для мужчин
+                    </p>
+                </div>
+            </div>
+            <div
+                class="relative h-89 w-89 overflow-hidden rounded-xl bg-[url(/resources/images/examp-3.jpg)]"
+            >
+                <div
+                    class="absolute top-0 right-0 bottom-0 left-0 flex items-end bg-black/25"
+                >
+                    <p class="mb-4 ml-5 text-lg font-semibold text-white">
+                        🔥 Любителям котиков
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
-<style scoped></style>
+<style scoped>
+h2 {
+    font-family: "Commissioner";
+}
+</style>
