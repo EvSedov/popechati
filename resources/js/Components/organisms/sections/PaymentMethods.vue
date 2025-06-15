ca840ef3bdf6837422e9ac7cec2a4201f7c10872
sedovev <sedovev@rgn.gov.ru>
2025-06-15 22:29:05 +0700

    adds adaptive for PaymentMethods

--- resources/js/Components/organisms/sections/PaymentMethods.vue
+++ resources/js/Components/organisms/sections/PaymentMethods.vue
@@ -15,40 +15,44 @@ const cards = [
         className: "w-15.5 h-10.5",
     },
 ];
-const wallets = [
-    {
-        name: "WebMoney",
-        icon: "/icons/webmoney.svg",
-        className: "w-27.5 h-8.5",
-    },
-    { name: "QIWI", icon: "/icons/qiwi.svg", className: "w-23.5 h-13" },
-    { name: "Юmoney", icon: "/icons/yoomoney.svg", className: "w-26 h-5.5" },
-];
-const terminals = [
-    { name: "Comepay", icon: "/icons/comepay.svg", className: "w-30 h-12.5" },
-    { name: "Связной", icon: "/icons/svyaznoy.svg", className: "w-28 h-9.5" },
-    { name: "Евросеть", icon: "/icons/euroset.svg", className: "w-28 h-7.5" },
-];
+// const wallets = [
+//     {
+//         name: "WebMoney",
+//         icon: "/icons/webmoney.svg",
+//         className: "w-27.5 h-8.5",
+//     },
+//     { name: "QIWI", icon: "/icons/qiwi.svg", className: "w-23.5 h-13" },
+//     { name: "Юmoney", icon: "/icons/yoomoney.svg", className: "w-26 h-5.5" },
+// ];
+// const terminals = [
+//     { name: "Comepay", icon: "/icons/comepay.svg", className: "w-30 h-12.5" },
+//     { name: "Связной", icon: "/icons/svyaznoy.svg", className: "w-28 h-9.5" },
+//     { name: "Евросеть", icon: "/icons/euroset.svg", className: "w-28 h-7.5" },
+// ];
 const bank = [{ name: "Банковский расчет", icon: "/icons/bank.svg" }];
 </script>
 
 <template>
     <section class="mx-auto my-0 flex w-full bg-[#f7f7f7] pt-18 pb-20">
-        <div class="mx-auto my-0">
-            <h2 class="mb-7 text-center text-[42px] font-bold">
+        <div class="mx-auto my-0 px-4 md:px-6 lg:px-8">
+            <h2
+                class="mb-7 text-center text-3xl font-bold sm:text-4xl md:text-[42px]"
+            >
                 Способы оплаты в Типографии
             </h2>
             <div
-                class="grid grid-cols-2 grid-rows-1 rounded-2xl bg-white text-sm"
+                class="grid grid-cols-1 overflow-hidden rounded-2xl bg-white text-sm xl:grid-cols-2"
             >
                 <!-- 1 -->
                 <div
-                    class="flex flex-col items-center justify-center border-r border-b border-[#ececec] px-19 pb-13"
+                    class="flex flex-col items-center justify-center border-b border-[#ececec] px-4 pb-13 xl:border-r xl:border-b-0 xl:px-19"
                 >
-                    <div class="mt-6.5 mb-6 font-medium">
+                    <div class="mt-6.5 mb-6 text-center font-medium">
                         Любые банковские карты
                     </div>
-                    <div class="flex items-center gap-10">
+                    <div
+                        class="flex flex-wrap items-center justify-center gap-10"
+                    >
                         <img
                             v-for="item in cards"
                             :key="item.name"
@@ -60,9 +64,9 @@ const bank = [{ name: "Банковский расчет", icon: "/icons/bank.sv
                 </div>
                 <!-- 2 -->
                 <div
-                    class="flex flex-col items-center justify-center border-r border-[#ececec] px-19 pb-13"
+                    class="flex flex-col items-center justify-center px-4 pb-13 xl:px-19"
                 >
-                    <div class="mt-6.5 mb-6 font-medium">
+                    <div class="mt-6.5 mb-6 text-center font-medium">
                         Оплата наличными и банковский перевод
                     </div>
                     <div class="flex items-center gap-8">
