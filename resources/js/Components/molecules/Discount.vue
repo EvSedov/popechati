c35f2ad8a93e127cc5fdb0b2e8d27b9e236033e6
sedovev <sedovev@rgn.gov.ru>
2025-06-18 16:06:33 +0700

    changes Discount

--- resources/js/Components/molecules/Discount.vue
+++ resources/js/Components/molecules/Discount.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
+import { inject } from "vue";
 import { Button } from "@/Components/atoms/ui/button";
+const openOrderModal = inject("openOrderModal") as () => void;
 </script>
 
 <template>
     <div class="bg-linear-to-r from-[#EA5A55] to-[#254EA3] py-[11px]">
         <div class="discount mx-auto flex-col xl:flex-row">
             <p
-                class="mb-2 text-center font-[Commissioner] text-xs/5.5 font-bold tracking-normal text-white xl:mb-0"
+                class="mb-2 text-center font-[Commissioner] text-xl/5.5 font-bold tracking-normal text-white uppercase xl:mb-0"
             >
-                Скидка 10% на онлайн заказ через корзину и бесплатная курьерская
-                доставка по Москве в пределах МКАД при заказе от 6 000 000 ₽!
+                Скидка 10% на первый заказ!
             </p>
             <Button
                 class="m-0 ml-[11.6px] h-[25px] bg-white px-[10px] py-0 text-[10px] text-[#2C2C2C] hover:bg-gray-300"
-                >Подробнее</Button
+                @click="openOrderModal"
+                >Сделать заказ</Button
             >
         </div>
     </div>
