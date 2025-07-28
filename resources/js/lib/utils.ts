2fd5e59fc30ed26009e9d0e60f87fd8af69e5fa7
sedovev <sedovev@rgn.gov.ru>
2025-07-28 20:56:27 +0700

    adds: добавлена функция прокрутки к секциям

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,6 +1,22 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
+
+export const scrollToSection = (targetId: string) => {
+    if (targetId) {
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            const elementPosition =
+                targetElement.getBoundingClientRect().top + window.scrollY;
+            const offsetPosition = elementPosition;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: "smooth",
+            });
+        }
+    }
+};
