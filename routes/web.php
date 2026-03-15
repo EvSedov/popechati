6d003c84117b3afed1ffc5738fe3d447b2b4ea08
sedovev <sedovev@rgn.gov.ru>
2026-03-15 13:50:19 +0700

    routes: добавлен throttle middleware для отправки заказа
    - Добавлен импорт OrderController
    - Применён middleware 'throttle:orders' к маршруту /submit-order
    - Защита от частых отправок формы (5 запросов в минуту)

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\OrderController;
 
 Route::get('/', function(){
     return Inertia::render('Home');
 });
 
-// Новый маршрут для отправки формы заказа (Изменено: Добавлен новый маршрут)
-Route::post('/submit-order', [App\Http\Controllers\OrderController::class, 'submitOrder']);
+// Новый маршрут для отправки формы заказа (с троттлингом)
+Route::post('/submit-order', [OrderController::class, 'submitOrder'])->middleware('throttle:orders');
