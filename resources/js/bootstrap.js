67d8429b4689fea56b153fe90a927f8ad70fb658
sedovev <sedovev@rgn.gov.ru>
2026-03-15 13:49:28 +0700

    bootstrap: настройка CSRF и JSON заголовков для axios
    - Добавлено автоматическое получение CSRF-токена из meta-тега
    - Установка X-CSRF-TOKEN заголовка для всех запросов
    - Добавлен Accept: application/json для JSON ответов Laravel

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,6 +7,13 @@ import {
 window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+// Ensure CSRF and JSON Accept headers are set for Laravel
+const csrf = document.head.querySelector('meta[name="csrf-token"]');
+if (csrf && csrf.getAttribute("content")) {
+    window.axios.defaults.headers.common["X-CSRF-TOKEN"] =
+        csrf.getAttribute("content");
+}
+window.axios.defaults.headers.common["Accept"] = "application/json";
 
 // Настройка обработки ошибок
 setupGlobalErrorHandler();
