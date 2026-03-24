f99be0105a2217e42e8f662f2acdcfdd1bc9b0f5
Evgeniy Sedov <sedoff-online@yandex.ru>
2026-03-24 18:46:52 +0700

    feat(ui): заменяет логотип на svg

--- resources/js/Components/atoms/Logo.vue
+++ resources/js/Components/atoms/Logo.vue
@@ -1,3 +1,3 @@
 <template>
-    <img src="/public/LogoPopechati.png" alt="Логотип Popechati" />
+    <img :src="'/LogoPopechati.svg'" alt="Логотип Popechati" class="block h-12 w-auto md:h-16" />
 </template> 
